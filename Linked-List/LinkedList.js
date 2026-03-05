966af762e197dd77ca9ea1b871156e2dce4ef56d
yashcoding07 <yash07coding@gmail.com>
2026-03-05 11:03:39 +0530

    feat: update LinkedList implementation to manage size during insertions and deletions

--- Linked-List/LinkedList.js
+++ Linked-List/LinkedList.js
@@ -9,17 +9,20 @@ class Node{
 class LinkedList{
     constructor(){
         this.head = null;
+        this.size = 0;
     }
 
     insertAtFirst(val){
         const newNode = new Node(val);
         if(this.head === null){
             this.head = newNode;
+            this.size++;
             return;
         }
 
         newNode.next = this.head;
         this.head = newNode;
+        this.size++;
     };
 
     deleteAtFirst(){
@@ -27,6 +30,7 @@ class LinkedList{
             console.log("Empty List");
         }else{
             this.head = this.head.next;
+            this.size--;
         }
     }
 
@@ -34,6 +38,7 @@ class LinkedList{
         const NewNode = new Node(val);
         if(this.head == null){
             this.head = NewNode;
+            this.size++;
             return;
         }else{
             let temp = this.head;
@@ -41,6 +46,7 @@ class LinkedList{
                 temp = temp.next;
             }
             temp.next = NewNode;
+            this.size++;
         }
     };
 
@@ -56,8 +62,47 @@ class LinkedList{
                     temp = temp.next;
                 }
                 temp.next = temp.next.next;
+                this.size--;
             }
         }  
+    }
+
+    insertAtIndex(val, index){
+        if(index < 0 || index > this.size){
+            console.log("Not Possible");
+            return;
+        }
+        let newNode = new Node(val);
+        this.size++;
+        if(this.head == null){
+            this.head = newNode;
+            return;
+        }
+        let temp = this.head;
+
+        for(let i = 0; i < index - 1; i++){
+            temp = temp.next;
+        }
+        newNode.next = temp.next;
+        temp.next = newNode;        
+    }
+
+    deleteAtIndex(index){
+        if(index < 0 || index >= this.size){
+            console.log("Invalid index");
+            return;            
+        }
+
+        if(this.head == null){
+            console.log("list is empty.");
+            return;
+        }
+        this.size--;
+        let temp = this.head;
+        for(let i = 0; i < index - 1; i++){
+            temp = temp.next;
+        }
+        temp.next = temp.next.next;
     }
 
     printll(){
@@ -82,11 +127,15 @@ ll.insertAtFirst(20);
 ll.insertAtFirst(30);
 ll.insertAtFirst(40);
 ll.insertAtFirst(50);
+// ll.printll();
+// ll.insertAtLast(100);
+// ll.insertAtLast(102);
+// ll.insertAtLast(103);
+// ll.printll();
+// ll.deleteAtFirst();
+// ll.deleteAtLast();
+// ll.printll();
+// ll.insertAtIndex(1000, 14);
 ll.printll();
-ll.insertAtLast(100);
-ll.insertAtLast(102);
-ll.insertAtLast(103);
-ll.printll();
-ll.deleteAtFirst();
-ll.deleteAtLast();
+ll.deleteAtIndex(3);
 ll.printll();
